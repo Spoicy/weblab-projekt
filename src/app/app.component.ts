ca98b027766c8e9e2ecf331b8a7d044e830a62c3
Jae <mrssfireball@gmail.com>
2024-02-21 11:18:39 +0100

    Update main page to have nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'weblab-projekt';
+
+  current: string = 'view';
+
+  changeView(view: string) {
+    this.current = view;
+  }
 }
